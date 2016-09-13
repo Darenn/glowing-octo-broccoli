108a5cc515f527a592c7d55710892479d5fd9153
Darenn Keller <keller.darenn@gmail.com>
2016-09-13 09:35:44 +0200

    J'ai codé la plupart des fonctions de chaine.c mais je n'ai pas testé.

--- chaine.c
+++ chaine.c
@@ -12,47 +12,79 @@ struct chaine {
 
 chaine chaine_creer_vide()
 {
+  chaine c = malloc(sizeof(chaine));
+  c->taille = 0;
+  c->tab = NULL;
+  return c;
 }
 
 chaine chaine_creer_char(char* c)
 {
+  chaine ch = malloc(sizeof(chaine));
+  ch->taille = strlen(c);
+  /* Je copie en mémoire la chaine donnée car je ne sais pas quand
+  il va la libérer */
+  char* tab = malloc (sizeof(char) * (ch->taille + 1)); // +1 pour le caractère NULL
+  strcpy(tab, c); // Copie aussi le caractère NULL
+  ch->tab = tab;
+  return ch;
 }
 
 void chaine_detruire(chaine* ch)
 {
+  // TODO
+  //free(ch->tab); // Libére le tableau ? Je ne comprends pas pourquoi ça ne fonctionne pas
+  free(ch); // Libére la struct
 }
 
 void chaine_afficher(FILE* f, chaine ch)
 {
+  char* c = ch->tab;
+  while (*c != 0) {
+    printf("%c", *c);
+    c++;
+  }
+  printf("\n");
 }
 
 unsigned int chaine_extraire_taille(chaine ch)
 {
+  return ch->taille;
 }
 
 
 bool chaine_est_vide(chaine ch)
 {
+  return ch->taille == 0 && ch->tab == NULL;
 }
 
 bool chaine_est_egal(chaine ch1, chaine ch2)
 {
+  return (!strcmp(ch1->tab, ch2->tab));
 }
 
 void chaine_concatener(chaine ch1, chaine ch2)
 {
+  strcat(ch1->tab, ch2->tab);
 }
 
 char chaine_extraire_char_i(chaine ch, const unsigned int i)
 {
+  return ch->tab[i];
 }
 
 void chaine_modifier_char_i(chaine ch, const unsigned int i, const char c)
 {
+  ch->tab[i] = c;
 }
 
 chaine chaine_copier(chaine ch1)
 {
+  chaine c = malloc(sizeof(chaine));
+  c->taille = ch1->taille;
+  char* tab = malloc(sizeof(char) * (c->taille + 1)); // +1 pour le caractère NULL
+  strcpy(tab, ch1->tab);
+  return c;
 }
 
 void chaine_en_minuscules(chaine ch)
@@ -70,8 +102,3 @@ bool chaine_appartenir(const char c, chaine ch, int* i)
 chaine chaine_lire(FILE* f, unsigned int taille)
 {
 }
-
-
-
-
-
--- test_chaines.c
+++ test_chaines.c
@@ -10,8 +10,7 @@
 
 int main (void ) {
 
-
-  FILE* f_in = fopen("test_chaines_in.txt","r");  
+  FILE* f_in = fopen("test_chaines_in.txt","r");
   FILE* f_out = fopen("test_chaines_out.txt","w");
   char a_lire[SIZE];
   fgets(a_lire,SIZE,f_in);
@@ -19,37 +18,37 @@ int main (void ) {
   chaine ch1 = chaine_creer_char(a_lire);
   fprintf(f_out,"ch1=");
   chaine_afficher(f_out,ch1);
-  
+
   chaine ch2 = chaine_creer_char("etidiante");
   fprintf(f_out,"ch2=");
   chaine_afficher(f_out,ch2);
-  
+
   chaine_modifier_char_i(ch2,2,'u');
   fprintf(f_out,"ch2 corrigé=");
   chaine_afficher(f_out,ch2);
-  
+
   chaine ch3 = chaine_copier(ch2);
   fprintf(f_out,"ch3=");
   chaine_afficher(f_out,ch3);
-  
+
   chaine_en_majuscules(ch3);
   fprintf(f_out,"ch3=");
   chaine_afficher(f_out,ch3);
-  
+
   chaine_concatener(ch3,ch1);
   fprintf(f_out,"ch3 modifié=");
   chaine_afficher(f_out, ch3);
-  
+
   chaine ch4 = chaine_creer_vide();
   chaine_concatener(ch4,ch2);
   fprintf(f_out,"ch4=");
   chaine_afficher(f_out, ch4);
-  
+
   chaine_detruire(&ch1);
   chaine_detruire(&ch2);
   chaine_detruire(&ch3);
   chaine_detruire(&ch4);
-  
+
   fclose(f_in);
   fclose(f_out);
 
