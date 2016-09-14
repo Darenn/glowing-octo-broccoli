f960912fac0e59bb63954417596f911e88c66668
Nicolas Hiot <nicolas.hiot@etu.univ-orleans.fr>
2016-09-14 17:46:20 +0200

    Ajout fonction "chaine_code" (pas de characteres double)

--- motus.c
+++ motus.c
@@ -17,23 +17,26 @@ struct motus_motus {
 
 motus motus_creer(unsigned int _t_mot, unsigned int _nb_essai, char* mot)
 {
-  motus m = (motus) malloc(sizeof(motus));
+  motus m = (motus) malloc(sizeof(struct motus_motus)); // Allocation memoire
+  // INIT
   m->t_mot = _t_mot;
   m->nb_essai = _nb_essai;
   m->mot = chaine_creer_char(mot);
   m->propositions = chaine_creer_vide();
   m->resultats = chaine_creer_vide();
+
   return m;
 }
 
 void motus_detruire(motus* m)
 {
+  // Destruction des trois chaîne
   chaine_detruire(&(*m)->mot);
   chaine_detruire(&(*m)->propositions);
   chaine_detruire(&(*m)->resultats);
+  // Destruction du jeu
   free(*m);
   m = NULL;
-  return;
 }
 
 
@@ -47,39 +50,70 @@ void motus_afficher(motus m, unsigned int numero, bool gagne)
       char c1 = chaine_extraire_char_i((m->propositions),i*m->t_mot+j);
       char c2 = chaine_extraire_char_i((m->resultats),i*m->t_mot+j);
       switch(c2) {
-      case CHAR_BIEN_PLACE:
-	couleur("37");
-	fprintf(stdout,"%c",c1);
-	couleur("0");
-	break;
-      case CHAR_MAL_PLACE:
-	couleur("34");
-	fprintf(stdout,"%c",c1);
-	couleur("0");
-	break;
-      case CHAR_NON_PRESENT:
-	couleur("31");
-	fprintf(stdout,"%c",c1);
-	couleur("0");
+        case CHAR_BIEN_PLACE:
+        couleur("37");
+        fprintf(stdout,"%c",c1);
+        couleur("0");
+        break;
+        case CHAR_MAL_PLACE:
+        couleur("34");
+        fprintf(stdout,"%c",c1);
+        couleur("0");
+        break;
+        case CHAR_NON_PRESENT:
+        couleur("31");
+        fprintf(stdout,"%c",c1);
+        couleur("0");
       }
     }
     fprintf(stdout,"\n");
   }
   for (unsigned int i=nb_resultats; i<m->nb_essai; i++) {
     for (unsigned int j=0;  j<m->t_mot; j++)
-      fprintf(stdout,CHAR_EN_ATTENTE);
+    fprintf(stdout,"%c",CHAR_EN_ATTENTE);
     fprintf(stdout,"\n");
   }
   if ((numero-1 != m->nb_essai) && (!gagne))
-    fprintf(stdout,"Proposition numéro %d : ",numero);
+  fprintf(stdout,"Proposition numéro %d : ",numero);
 }
 
 chaine chaine_code(chaine ch, chaine mot, bool* gagne)
 {
-  return ch;
+  // Unification des characteres
+  chaine_en_minuscules(ch);
+  chaine_en_minuscules(mot);
+
+  // Creation de la chaine code
+  chaine code = chaine_copier(mot); // Même taille que le mot a trouver
+  for (unsigned int i = 0; i < chaine_extraire_taille(code); i++) {
+    chaine_modifier_char_i(code, i, CHAR_EN_ATTENTE);
+  }
+
+  // Variables
+  int j;  // Indice du charactere si trouvé
+  char c; // Charactere courant
+
+  // On parcourt toute la chaine proposé
+  for (unsigned int i = 0; i < chaine_extraire_taille(ch); i++) {
+    c = chaine_extraire_char_i(ch, i); // Recuperation du charactere courant
+
+    if (chaine_appartenir(c, mot, &j)) { // Si le charactere appartient au mot
+      if (j == i) { // Si il est bien placé
+        // TODO: correction conparaison entre int et uint
+        chaine_modifier_char_i(code, i, CHAR_BIEN_PLACE);
+      } else { // Sinon
+        chaine_modifier_char_i(code, i, CHAR_MAL_PLACE);
+      }
+    } else { // Si le charactere n'appartient pas au mot
+      chaine_modifier_char_i(code, i, CHAR_NON_PRESENT);
+    }
+  }
+
+  *gagne = chaine_est_egal(ch, mot); // Test de victoire
+  return code; // Retour du code
 }
 
 void motus_jeu(motus m)
 {
-  return m;
+
 }
